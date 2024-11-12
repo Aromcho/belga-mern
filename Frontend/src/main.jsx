9649a6042756f5f83da53485563c6e7acf5bd0aa
Aroncho <barriosarom@gmail.com>
2024-11-12 13:03:20 -0300

    arreglando el seo de whatsapp

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App.jsx';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ReactGA from 'react-ga4';
@@ -11,9 +12,10 @@ ReactGA.initialize('G-JQJ99XGWYV'); // Reemplaza con tu código de seguimiento G
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <HelmetProvider>
+      <Router>
+        <App />
+      </Router>
+    </HelmetProvider>
   </StrictMode>
 );
-  
